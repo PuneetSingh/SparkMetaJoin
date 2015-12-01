45cb7f4ef1aa8ed7ff3d497545171e377213dfa7
Puneet Singh <puneet.singh@kronos.com>
2015-12-01 09:55:00 +0530

    Parser Inroduced

---
+++ src/main/java/org/apache/sparkmeta/join/helper/MetaDataParser.java
@@ -0,0 +1,10 @@
+package org.apache.sparkmeta.join.helper;
+
+import org.apache.sparkmeta.join.vo.MetaData;
+
+public class MetaDataParser {
+
+	public MetaData getMetaData() {
+		return null;
+	}
+}
