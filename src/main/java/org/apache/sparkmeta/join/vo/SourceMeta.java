e44b01451c056c499309bb2856c258f02ad485b8
Puneet Singh <puneet.singh@kronos.com>
2015-12-02 13:52:57 +0530

    Updates

--- src/main/java/org/apache/sparkmeta/join/vo/SourceMeta.java
+++ src/main/java/org/apache/sparkmeta/join/vo/SourceMeta.java
@@ -14,6 +14,12 @@ public class SourceMeta {
 	// Cassandra
 	private String cassandraKeyspace;
 	private String cassandraTablename;
+	
+	//Database
+	private String hostName;
+	private String port;
+	private String database;
+	private String table;
 
 	public SourceType getSourceType() {
 		return sourceType;
@@ -21,5 +27,77 @@ public class SourceMeta {
 
 	public void setSourceType(SourceType sourceType) {
 		this.sourceType = sourceType;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public boolean isFileHasHeader() {
+		return fileHasHeader;
+	}
+
+	public void setFileHasHeader(boolean fileHasHeader) {
+		this.fileHasHeader = fileHasHeader;
+	}
+
+	public String getCassandraKeyspace() {
+		return cassandraKeyspace;
+	}
+
+	public void setCassandraKeyspace(String cassandraKeyspace) {
+		this.cassandraKeyspace = cassandraKeyspace;
+	}
+
+	public String getCassandraTablename() {
+		return cassandraTablename;
+	}
+
+	public void setCassandraTablename(String cassandraTablename) {
+		this.cassandraTablename = cassandraTablename;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getTable() {
+		return table;
+	}
+
+	public void setTable(String table) {
+		this.table = table;
 	}
 }
